3fcfc96806e4a196bc820c170acc39cbc75b286b
juancrespo25 <jcrespovirhuez@gmail.com>
2023-06-04 20:59:08 -0500

    add my vpc

---
+++ environments/prod.tfvars
@@ -0,0 +1,2 @@
+key_pair_name = "cloudcamp-key-pair"
+cidr = "10.28.0.0/16"
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "key_pair_name" {
+  description = "The name of the key pair"
+  type        = string
+}
+
+variable "cidr" {
+  description = " The CIDR of the VPC"
+  type        = string
+}
