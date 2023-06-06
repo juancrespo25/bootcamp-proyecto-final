2c5b7b80a9a4534ef55cdf07327eaa24a4b16412
juancrespo25 <jcrespovirhuez@gmail.com>
2023-06-05 23:49:29 -0500

    feat: add terraform fmt and terraform validate 4

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
 provider "aws" {
   region = "us-east-2"
 }
-
-/*
-terraform {
-  backend "s3" {
-    bucket = "cloudcamp-terraform-state-jcv"
-    key    = "workspaces_proyecto/terraform.tfstate"
-    region = "us-east-2"
-    dynamodb_table = "cloudcamp-ddb-lock"
-    encrypt = true
-  }
-}*/
